38a40c413ab31b317190ae39343837a568f140e5
cindro <info@cindro.com>
2019-11-20 20:31:59 +0100

    Create LipaResponse.cs

---
+++ MpesaSDK.NET/Dtos/Responses/LipaResponse.cs
@@ -0,0 +1,19 @@
+using Newtonsoft.Json;
+
+namespace MpesaSDK.NET.Dtos.Responses
+{
+    public class LipaResponse
+    {
+        [JsonProperty("IsSuccess")]
+        public bool IsSuccess { get; set; }
+        [JsonProperty("SuccessResponse")]
+        public StkPushSuccessResponse SuccessResponse { get; set; }
+        [JsonProperty("ErrorResponse")]
+        public ErrorResponse ErrorResponse { get; set; }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
